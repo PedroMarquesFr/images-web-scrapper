d040930bf3c194ec92e0c037afcec1ae2436bb1c
Pedro Marques Fr <pedromarques1011@gmail.com>
2021-03-24 17:16:43 -0300

    feito api

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require("express");
+const getPhotoByName = require("./script");
+var cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`- ${req.method} ${req.path}`);
+  /* Termina a operação no middleware e chama o próximo middleware ou rota */
+  next();
+});
+
+app.get("/:termForSearch", async (req, res) => {
+  const { termForSearch } = req.params;
+  const photosArray = await getPhotoByName(termForSearch);
+  res.json({ resp: photosArray });
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`listen port: ${process.env.PORT}`)
+);
---
+++ script.js
@@ -0,0 +1,26 @@
+const cheerio = require("cheerio");
+const axios = require("axios");
+
+function getPhotoByName(name) {
+  return axios
+    .get(
+      `https://www.google.com/search?hl=en&ie=utf-8&q=${name}&filter=0&tbm=isch`
+    )
+    .then(
+      (res) => {
+        const images = [];
+        if (res.status === 200) {
+          const html = res.data;
+          const $ = cheerio.load(html);
+
+          $("img").each((index, image) => {
+            const img = $(image).attr("src");
+            images.push(img);
+          });
+          return images;
+        }
+      },
+      (err) => console.log(err)
+    );
+}
+module.exports = getPhotoByName;
